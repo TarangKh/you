9f10d0c88486b6897c96b933d757bfc7b640f994
Tarang Kharkar <110414998+TarangKh@users.noreply.github.com>
2024-03-30 01:24:21 +0530

    Add files via upload
    first version

---
+++ script.js
@@ -0,0 +1,53 @@
+
+function myFunc(event) {
+    let val = event.target.value;
+    console.log(val);
+    let img = document.querySelector("#bg_img");
+    let v = document.querySelector(".video");
+    let vd = document.querySelector(".main-video");
+    
+    if (val == "144p") {
+        vd.style.visibility = "hidden";
+        vd.pause();
+        vd.currentTime = 0;
+        img.src = "src/moon1.jpeg";
+        img.style.visibility = "visible";
+        v.style.visibility = "hidden";
+    }
+    else if (val == "360p") {
+        vd.style.visibility = "hidden";
+        vd.pause();
+        vd.currentTime = 0;
+        img.src = "src/moon4.jpeg";
+        img.style.visibility = "visible";
+        v.style.visibility = "hidden";
+    }
+    else if (val == "720p") {
+        vd.style.visibility = "hidden";
+        vd.pause();
+        vd.currentTime = 0;
+        img.src = "src/moon3.jpeg";
+        img.style.visibility = "visible";
+        v.style.visibility = "hidden";
+    }
+    else if (val == "1080p") {
+        
+        vd.style.visibility = "hidden";
+        vd.pause();
+        vd.currentTime = 0;
+        img.src = "src/moon2.jpeg";
+        img.style.visibility = "visible";
+        v.style.visibility = "hidden";
+    }
+    else {
+        let fr = document.querySelector(".frame");
+        let form = document.querySelector(".form");
+        fr.style.backgroundColor = "black";
+        form.style.backgroundColor = "#ccc";
+        v.style.visibility = "visible";
+        img.style.visibility = "hidden";
+        vd.style.visibility = "visible";
+        vd.play();
+    }
+    // vd.remove();
+}
